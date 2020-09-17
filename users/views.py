3638a038626ef7adfcf2c8b11745b9f90660bd32
Dami Osayomi <Damio@users.noreply.github.com>
2020-09-17 12:38:42 -0400

    storing app on GitHub

---
+++ users/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+#authentication
+from django.contrib.auth import authenticate, login, logout
+
+# Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    return render(request, "users/user.html")
+
+
+def login_request(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        #authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html",{
+                "message": "Invalid credentials, please try again."
+            })
+    return render(request, "users/login.html")
+
+
+def logout_request(request):
+    logout(request)
+
+    return render(request, "users/login.html",{
+        "message": "You have successfully logged out"
+    })
+
+    
